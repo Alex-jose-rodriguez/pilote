1de1697b48ee1eed3c38c30f28c10cabb3317b54
alex <rodriquez.alex@gmail.com>
2020-02-04 15:55:01 -0500

    Nuevo proyecto tablero de objetos

--- src/main/java/main.java
+++
@@ -1,8 +0,0 @@
-import com.lacroix.lacroixnet.controller.LanguageController;
-
-public class main {
-    public static void main(String [] args){
-        LanguageController languageController = new LanguageController();
-        languageController.sampleDetailLanguage();
-    }
-}
